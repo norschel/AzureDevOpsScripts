656745b42149fa1ae8abb5e594065b582cd30a3d
Nico Orschel <norschel@users.noreply.github.com>
2022-05-07 14:39:53 +0200

    Add link for installing azure devops cli

--- Migration/install_collection_of_extensions_from_source_org.ps1
+++ Migration/install_collection_of_extensions_from_source_org.ps1
@@ -1,4 +1,5 @@
 # Install a collection of Azure DevOps extensions from a source organization into a target extension
 # Prerequisites: azure cli (az), az devops extension, powershell
+# Install Azure DevOps CLI: https://docs.microsoft.com/en-us/azure/devops/cli/?view=azure-devops
 
 az devops extension list --organization https://dev.azure.com/<source> | ConvertFrom-Json | select extensionId,publisherId | where {$_.publisherId -ne 'ms'} | % {& az devops extension install --extension-id $_.extensionId --publisher-id $_.publisherId --organization https://dev.azure.com/<target>}
