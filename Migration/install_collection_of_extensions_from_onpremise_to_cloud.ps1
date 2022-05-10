d29f25efc237b00b5d7af4dfb5a8ddcb5cb20b66
Nico Orschel <norschel@users.noreply.github.com>
2022-05-10 10:11:55 +0200

    Install a collection of Azure DevOps extensions from onpremise AzD server (source) into a Azure DevOps service (target) cloud orga

---
+++ Migration/install_collection_of_extensions_from_onpremise_to_cloud.ps1
@@ -0,0 +1,14 @@
+# Install a collection of Azure DevOps extensions from onpremise AzD server (source) into a Azure DevOps service (target) cloud orga
+# Prerequisites: azure cli (az), az devops extension, powershell
+# Install Azure DevOps CLI: https://docs.microsoft.com/en-us/azure/devops/cli/?view=azure-devops
+
+#  1. Retrieve extension list via Database SQL Query:
+#      SELECT
+#      [PublisherName]
+#      ,[ExtensionName]
+#      FROM [<CollectionDBName>].[Extension].[tbl_InstalledExtension]
+#      where not PublisherName = 'ms'
+#  2. Save query output as CSV file
+#  3. Install Extensions using Azure CLI
+
+Import-Csv .\Extensions.csv -Delimiter ";" | where {$_.publisherId -ne 'ms'} | % {& az devops extension install --extension-id $_.ExtensionName --publisher-id $_.PublisherName --organization https://dev.azure.com/<target>}
