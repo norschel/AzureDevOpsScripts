d994bd822a0c4279e4e1a4479d49b9acaeffc041
Nico Orschel <norschel@users.noreply.github.com>
2025-05-07 16:07:12 +0200

    Update install_collection_of_extensions_from_onpremise_to_cloud.ps1

--- Migration/install_collection_of_extensions_from_onpremise_to_cloud.ps1
+++ Migration/install_collection_of_extensions_from_onpremise_to_cloud.ps1
@@ -3,12 +3,13 @@
 # Install Azure DevOps CLI: https://docs.microsoft.com/en-us/azure/devops/cli/?view=azure-devops
 
 #  1. Retrieve extension list via Database SQL Query:
-#      SELECT
-#      [PublisherName]
-#      ,[ExtensionName]
-#      FROM [<CollectionDBName>].[Extension].[tbl_InstalledExtension]
-#      where not PublisherName = 'ms'
-#  2. Save query output as CSV file
+      SELECT
+      [PublisherName]
+      ,[ExtensionName]
+      FROM [<CollectionDBName>].[Extension].[tbl_InstalledExtension]
+      where not PublisherName = 'ms'
+#  2. Save query output as CSV file and add header "PublisherName;ExtensionName" to that file 
 #  3. Install Extensions using Azure CLI
-
-Import-Csv .\Extensions.csv -Delimiter ";" | where {$_.publisherId -ne 'ms'} | % {& az devops extension install --extension-id $_.ExtensionName --publisher-id $_.PublisherName --organization https://dev.azure.com/<target>}
+      az login
+      az devops login
+      Import-Csv .\Extensions.csv -Delimiter ";" | where {$_.publisherId -ne 'ms'} | % {& az devops extension install --extension-id $_.ExtensionName --publisher-id $_.PublisherName --organization https://dev.azure.com/<target>}
